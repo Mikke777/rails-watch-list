19a6f3f56a8a00417a76a36951e8454e5a930b0a
michele di stadio <distadiomichele93@gmail.com>
2024-09-18 11:56:07 +0200

    schema -migration

--- db/migrate/20240916155137_add_user_to_lists.rb
+++ db/migrate/20240916155137_add_user_to_lists.rb
@@ -1,5 +1,12 @@
 class AddUserToLists < ActiveRecord::Migration[7.1]
   def change
-    add_reference :lists, :user, null: false, foreign_key: true
+    add_reference :lists, :user, null: true, foreign_key: true
+
+    # Backfill user_id for existing records (adjust based on your logic)
+    List.reset_column_information
+    List.update_all(user_id: User.first.id) # Assign to the first user or a default user
+
+    # Then, make it not nullable after backfilling
+    change_column_null :lists, :user_id, false
   end
 end
